0f416a15816fd1c8540658ad8a505d94ccc92884
rocklee <445130792@qq.com>
2017-07-24 12:29:19 +0800

    添加github依懒

---
+++ LibModul/src/main/java/com/rock/lee/librockleeframe/lm/base/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.rock.lee.librockleeframe.lm.base;
+
+import android.app.Activity;
+import android.util.Log;
+
+/**
+ * Created by rock.lee on 2017/7/24.
+ * I am very NB ，NB very much !!!
+ */
+
+public class BaseActivity extends Activity {
+    
+    
+    public void ss() {
+        Log.e("rocklee","rocklee fuck you ");
+    }
+}
