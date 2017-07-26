12f5b9e0e12d6e81298c920e81bc67b5a58449f7
rocklee <445130792@qq.com>
2017-07-26 15:56:02 +0800

    初始化，导入log输入框架和注解框架

--- LibModul/src/main/java/com/rock/lee/librockleeframe/lm/base/BaseActivity.java
+++ LibModul/src/main/java/com/rock/lee/librockleeframe/lm/base/BaseActivity.java
@@ -1,7 +1,10 @@
 package com.rock.lee.librockleeframe.lm.base;
 
 import android.app.Activity;
-import android.util.Log;
+import android.content.Intent;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by rock.lee on 2017/7/24.
@@ -9,9 +12,42 @@ import android.util.Log;
  */
 
 public class BaseActivity extends Activity {
+
+    /** 使用ButterKnife框架时，继承父类，调用时先设置布局文件 */
+    private int layoutId;
     
-    
-    public void ss() {
-        Log.e("rocklee","rocklee fuck you 2");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * 使用ButterKnife框架时，继承父类，调用时先设置布局文件
+     * @param layoutId
+     */
+    public void setLayoutId(int layoutId) {
+        this.layoutId = layoutId;
+    }
+
+
+    /**
+     * 页面跳转使用
+     * @param cls
+     * @return
+     */
+    public Intent getIntent(Class<?> cls) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        return intent;
+    }
+
+
+    /**
+     * 普通跳转页面
+     * @param cls
+     */
+    public void startClassActivity(Class<?> cls) {
+        startActivity(getIntent(cls));
     }
 }
